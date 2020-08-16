e0eac5ef9fd9b4548d7c3865dd5d74576bddd0e9
Alexandre <alexandrembonomi@gmail.com>
2020-08-15 21:11:55 -0300

    Change code 500 to code 400

--- server/routes/map.js
+++ server/routes/map.js
@@ -4,7 +4,7 @@ const googleAPI = require('../utils/googleAPI');
 router.post('/geocoder', (req, res) => {
   googleAPI.getGeocode(req.body.address)
   .then(result => res.status(200).json(result))
-  .catch(e => res.status(500).json(e));
+  .catch(e => res.status(400).json(e));
 });
 
 module.exports = router;
