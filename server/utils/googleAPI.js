db4e5dc341002a0020bc5ac2634935787475f26c
Alexandre <alexandrembonomi@gmail.com>
2020-08-15 21:07:40 -0300

    production google api

--- server/utils/googleAPI.js
+++ server/utils/googleAPI.js
@@ -4,11 +4,11 @@ const jsonData = require('./mockData');
 const getGeocode = (address) => {
   address = encodeURI(address);
   return new Promise((resolve, reject) => {
-    resolve(jsonData);
-    // axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.G_API_KEY}`
-    // )
-    // .then(result => resolve(result.data.results))
-    // .catch(e => reject(e));
+    // resolve(jsonData);
+    axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.G_API_KEY}`
+    )
+    .then(result => resolve(result.data.results))
+    .catch(e => reject(e));
   });
 };
 
